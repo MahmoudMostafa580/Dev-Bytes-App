b80a67901ab79d9ecc811cb39acc6527f9cb5246
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2024-08-10 17:28:58 +0300

    Add DatabaseVideo Entity

--- app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -17,3 +17,30 @@
 
 package com.example.android.devbyteviewer.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android.devbyteviewer.domain.Video
+
+@Entity
+data class DatabaseVideo constructor(
+    @PrimaryKey
+    val url: String,
+    val updated: String,
+    val title: String,
+    val description: String,
+    val thumbnail: String
+)
+
+fun List<DatabaseVideo>.asDomainModel(): List<Video> {
+    return map {
+        Video(
+            title = it.title,
+            url = it.url,
+            description = it.description,
+            updated = it.updated,
+            thumbnail = it.thumbnail
+            )
+    }
+}
+
+
